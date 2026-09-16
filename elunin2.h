5d0afd14837a185fbd9a1f4d0a4d13547294d514
Igor Elunin <elunin.igorr@yandex.ru>
2026-09-16 21:38:46 +0700

    Revert: удалены файлы elunin2.cpp и elunin2.h

--- elunin2.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELUNIN2_H
-#define ELUNIN2_H
-
-// Функции варианта 26 
-double work(double F, double s);
-double power(double A, double t);
-
-#endif 
--- main.cpp
+++ main.cpp
@@ -4,62 +4,24 @@
 using namespace std;
 
 // === БЛОК ПОДКЛЮЧЕНИЙ: каждый участник добавляет свой заголовочный файл ===
-#include "elunin1.h"
-#include "elunin2.h"
+// #include "elunin1.h"
+// #include "elunin2.h"
 // === КОНЕЦ БЛОКА ПОДКЛЮЧЕНИЙ ===
 
 int main() {
     int choice;
-    double v, f, lambda, T, v0, a, t, s, F, A;
+    double v, f, lambda, T, v0, a, t, s;
 
     do {
         cout << "\n=== Командный проект: сборник расчётов ===\n";
-        cout << "1. Distance (s = v * t)\n";
-        cout << "2. Speed (v = s / t)\n";
-        cout << "3. Travel time (t = s / v)\n";
-        cout << "4. Mechanical work (A = F * s)";
-        cout << "5. Power (N = A / t)";
-        cout << "0. Exit\n";
+        // === БЛОК МЕНЮ: каждый участник добавляет свои пункты ===
+        cout << "0. Выход\n";
+        // === КОНЕЦ БЛОКА МЕНЮ ===
         cout << "Выберите пункт: ";
         cin >> choice;
 
         switch (choice) {
-            case 1:
-                cout << "Enter speed v: ";
-                cin >> v;
-                cout << "Enter the time t: ";
-                cin >> t;
-                cout << "Distance = " << distance(v, t) << "\n";
-                break;
-                
-            case 2:
-                cout << "Enter the distance s: ";
-                cin >> s;
-                cout << "Enter the time t: ";
-                cin >> t;
-                cout << "Speed = " << speed(s, t) << "\n";
-                break;
-
-                
-            case 3:
-                cout << "Enter the distance s: ";
-                cin >> s;
-                cout << "Enter speed v: ";
-                cin >> v;
-                cout << "Travel time = " << travelTime(s, v) << "\n";
-                break;
-            case 4:
-                cout << "Enter the force F: ";
-                cin >> F;
-                cout << "Enter the distance s: ";
-                cin >> s;
-                cout << "Mechanical work = " << work(F, s) << "\n";
-            case 5:
-                cout << "Enter the Mechanical work A: ";
-                cin >> A;
-                cout << "Enter the time t: ";
-                cin >> t;
-                cout << "Power = " << power(A, t) << "\n";
+            // === БЛОК ОБРАБОТКИ: каждый участник добавляет свои case ===
             case 0:
                 cout << "Работа завершена.\n";
                 break;
