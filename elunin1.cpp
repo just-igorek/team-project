5e296c8c563bd2cf9fd431c0df6ed1851dbbc23b
Igor Elunin <elunin.igorr@yandex.ru>
2026-09-16 20:31:26 +0700

    feat: перенесены функции варианта 21, добавлены пункты меню 1-3

---
+++ elunin1.cpp
@@ -0,0 +1,24 @@
+#include "elunin1.h"
+#include <iostream>
+using namespace std;
+
+//1. Путь: s = v * t
+double distance(double v, double t){
+    return v * t;
+}
+//2. Скорость: v = s / t
+double speed(double s, double t){
+    if (t == 0){
+        cout << "Error: vrmya cannot be null\n";
+        return -1;
+    }
+    return s / t;
+}
+//3. Время: t = s / v
+double travelTime(double s, double v) {
+    if (v == 0) {
+        cout << "Error: Speed ​​cannot be zero\n";
+        return -1;
+    }
+    return s / v;
+}
